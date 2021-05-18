c1325551f25df65af641ec60a8d400f38725b741
LinkinWD <yaramodesu@gmail.com>
2021-05-18 11:37:40 +0300

    ventti valmis

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,10 @@ const peliAlue = document.querySelector('#pelialue')
 const tietokoneKortit = document.querySelector('#tietokonekortit')
 const pelaajaKortit = document.querySelector('#pelaajakortit')
 const h5 = document.querySelectorAll('h5')
+const voittoRuutu = document.querySelector('.voittoruutu')
 
 /* pelaajat */
 const pelaajanPisteet = document.querySelector('#pisteet')
-
-
 
 const pelaaja = 
   {
@@ -30,14 +29,17 @@ const tietokone =
     ässävähennyt: false
   }
 
-
 /* eventlistenerit */
 uusiPeli.addEventListener('click', aloitaPeli)
 vedäKortti.addEventListener('click', pelaajaVetääKortin)
 katso.addEventListener('click', pelaajaKatsoo)
 
+/* alkuasettelu ja button functiot */
+katso.disabled = true
+vedäKortti.disabled = true
 
 function aloitaPeli() {
+  document.body.style.backgroundColor = 'limegreen'
     pelaaja.pisteet = 0
     tietokone.pisteet = 0
     pelaaja.ässät = false
@@ -45,6 +47,9 @@ function aloitaPeli() {
     tietokoneKortit.innerHTML = ''
     pelaajaKortit.innerHTML = ''
     vedäKortti.disabled = false
+    katso.disabled = false
+    voittoRuutu.classList.add('piilota')
+    voittoRuutu.innerHTML = ''
     sekoita(korttipakka)
     jaaKortti(tietokone)
     jaaKortti(tietokone)
@@ -73,6 +78,7 @@ function pelaajaKatsoo() {
 
 /* muut functiot */
 
+/* Sekoita algorythm */
 function sekoita(array) {
   let currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -92,6 +98,7 @@ function sekoita(array) {
   return array;
 }
 
+/* pelifunctiot */
 function jaaKortti(puoli) {
     let sijainti
     let img = document.createElement('img')
@@ -121,27 +128,18 @@ function jaaKortti(puoli) {
 function tietokonePelaa() {
   while(tietokone.pisteet < 17) {
     jaaKortti(tietokone)
-    tarkistaMeniköYli(tietokone)
   }
+  tarkistaMeniköYli(tietokone)
   tarkistaVoittaja()
 }
 
 
-
-function piilotaKortti() {
-  tietokoneKortit.firstChild.style.zIndex = '-1'
-  let img = document.createElement('img')
-  img.classList.add('peitekortti')
-  img.src = '../img/purple_back.png'
-  tietokoneKortit.append(img)
-  img.style.position = 'absolute'
-} 
-
-
+/* voitto ja voitontarkistu functiot */
 function tarkistaMeniköYli(puoli) {
   if(puoli.pisteet > 21) {
-    let voittaja = (puoli.nimi === 'tietokone' ? 'pelaaja' : 'tietokone')
+    let voittaja = (puoli.nimi === 'tietokone') ? 'pelaaja' : 'tietokone'
     voittoSeremonia(voittaja)
+    return false
   }
 }
 
@@ -156,16 +154,34 @@ function tarkistaVoittaja() {
   }
   else if ( pPisteet > tPisteet) {
     voittoSeremonia('pelaaja')
+  } else if(tPisteet > 21) { 
+    voittoSeremonia('pelaaja')
   } else {
     voittoSeremonia('tietokone')
   }
 }
+
+
+function voittoSeremonia(voittaja) {
+  katso.disabled = true
+  vedäKortti.disabled = true
+  let väri = (voittaja === 'tietokone') ? 'darkgreen' : 'lightgreen'
+  document.body.style.backgroundColor = väri
+  voittoRuutu.classList.remove('piilota')
+  voittoRuutu.innerHTML = `<h2> ${voittaja} voitti Pelin!!!!</h2>`
+
+}
+/* apufunctiot */
 function näytäTekstit() {
   h5.forEach( function(kohde) {
     kohde.classList.remove('piilota')
   })
 }
-function voittoSeremonia(voittaja) {
-  let väri = (voittaja === 'tietokone') ? 'red' : 'yellow'
-  document.body.style.backgroundColor = väri
-}
+function piilotaKortti() {
+  tietokoneKortit.firstChild.style.zIndex = '-1'
+  let img = document.createElement('img')
+  img.classList.add('peitekortti')
+  img.src = '../img/purple_back.png'
+  tietokoneKortit.append(img)
+  img.style.position = 'absolute'
+} 
